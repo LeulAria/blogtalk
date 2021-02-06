69f6ce2f83e61476c5b2f0981a8cb7973b454eae
leularia <leulsegedtariku@gmail.com>
2021-02-06 14:08:59 +0300

    update whitenoise setting

--- blogtalk/settings.py
+++ blogtalk/settings.py
@@ -147,7 +147,7 @@ if DEBUG:
     INSTALLED_APPS.insert(0, "whitenoise.runserver_nostatic")
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ALLOWED_HOSTS = ['blogtalk-alpha.herokuapp.com']
